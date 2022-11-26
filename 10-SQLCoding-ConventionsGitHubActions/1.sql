92a0a973f51524c26ed24c3f5569a720e0fc41fb
poshtarenko <40522612+poshtarenko@users.noreply.github.com>
2022-11-26 18:25:20 +0200

    Update 1.sql

--- 10-SQLCoding-ConventionsGitHubActions/1.sql
+++ 10-SQLCoding-ConventionsGitHubActions/1.sql
@@ -1,1 +1,9 @@
-SELECT a1 FROM t1;
+CREATE TABLE Furniture (
+    Id NUMBER(9),
+    Name VARCHAR(30),
+    Type_Code NUMBER(3),
+    Producer VARCHAR(30),
+    Amount NUMBER(5),
+    Production_Time NUMBER(3), -- in days
+    Price NUMBER(9, 2)
+);

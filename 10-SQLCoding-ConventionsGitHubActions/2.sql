931152220c11f5ea635879b8139607788e1e40fa
poshtarenko <40522612+poshtarenko@users.noreply.github.com>
2022-11-27 15:08:00 +0200

    Update 2.sql

--- 10-SQLCoding-ConventionsGitHubActions/2.sql
+++ 10-SQLCoding-ConventionsGitHubActions/2.sql
@@ -1,3 +1,63 @@
+-- Помилки :
+-- L014	2 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	3 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L029	3 / 5	
+-- Keywords should not be used as identifiers.
+-- L014	4 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	10 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	11 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L029	11 / 5	
+-- Keywords should not be used as identifiers.
+-- L014	12 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	13 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	14 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	15 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	16 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L008	16 / 19	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	21 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L029	21 / 5	
+-- Keywords should not be used as identifiers.
+-- L014	22 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	23 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	24 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	28 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	29 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	30 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	31 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	32 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L008	32 / 27	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	34 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	38 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	39 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	40 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+-- L014	41 / 5	
+-- Unquoted identifiers must be consistently pascal case.
+
 CREATE TABLE Type (
     code NUMBER(3),
     name VARCHAR(30),
